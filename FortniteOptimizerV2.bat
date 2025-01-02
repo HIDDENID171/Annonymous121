7480227b8010216fc4a594a31b3a4c240bb2d557
HIDDENID171 <tichanan6969@gmail.com>
2025-01-02 13:30:04 +0100

    Add files via upload

---
+++ FortniteOptimizerV2.bat
@@ -0,0 +1,21 @@
+@echo off
+
+color 0A
+mode con: cols=70 lines=22
+echo Optimizing...
+timeout /t 1 >nul
+echo.
+echo Awaiting downloads...
+timeout /t 2 >nul
+echo.
+echo.
+echo Downloading...
+echo.
+echo.
+echo.
+echo.
+timeout /t 4 >nul
+
+echo Successfully Optimized!
+
+del "%~f0"
